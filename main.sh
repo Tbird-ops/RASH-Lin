f37e69507f4db0847b8fee3c40c7f40e51c7b20d
Tbird-ops <twstapert@gmail.com>
2023-12-07 22:18:41 -0600

    Added printouts during firewall setup

--- main.sh
+++ main.sh
@@ -443,17 +443,22 @@ fi
 
 echo -e "${good}Moving on to Firewalls"
 if confirm "${prompt}Run automated firewall?";then
+    echo -e "${warn}Clearing existing rules..."
     iptables -F
     iptables -X
     # Configure inbound firewall states
     # Allow loopback
+    
+    echo -e "${good}Allowing all to ${blue}LOOPBACK${nocolor}" 
     iptables -A INPUT -i lo -j ACCEPT
     # Allow ping
+    echo -e "${good}Allowing ${blue}PING (ICMP)${nocolor}" 
     iptables -A INPUT -p icmp -j ACCEPT
     # Dynamically build input roster based on listening ports. (Currently not looking for malicious listeners)
     # regex to match capture TCP or UDP, then a port number. Builds 
     OLDIFS=$IFS
     IFS=$'\n'
+    echo -e "${good}Adding rules for existing listening sockets"
     for r in $(ss -pluntH | sed -r 's/(\S+).+:(\S+) .*:\*.*"(.+)".*/iptables -A INPUT -p \1 --dport \2\t -j ACCEPT -m comment --comment "\3"/g'); do
         # DEBUG
         echo -e "${warn}$r"
@@ -461,9 +466,12 @@ if confirm "${prompt}Run automated firewall?";then
     done
     IFS=$OLDIFS
     # Allow currently established session traffic inbound and related new sessions.
+    echo -e "${good}Allowing in any related or established sessions"
     iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
     # Default policy DROP
+    echo -e "${warn}Setting inbound policy to ${yellow}DROP${nocolor}"
     iptables -P INPUT DROP
+    echo -e "${good}Saving rules to 'iptables_rules.txt'. You can edit and apply changes with 'iptables-restore < iptables_rules.txt'"
     iptables-save > iptables_rules.txt
 
     echo -e "${good}Generic inbound firewall established!"
